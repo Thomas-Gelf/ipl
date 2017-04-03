65a44e8ff97e8ce498df9b808c09585d9e294328
Thomas Gelf <thomas@gelf.net>
2017-04-03 23:17:44 +0200

    ipl\Html: initial import

---
+++ lib/ipl/Html/DeferredText.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ipl\Html;
+
+use Exception;
+
+class DeferredText implements Renderable
+{
+    /** @var callable */
+    protected $callback;
+
+    protected $escaped = false;
+
+    public function __construct(callable $callback)
+    {
+        $this->callback = $callback;
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public static function create(callable $callback)
+    {
+        return new static($callback);
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        $callback = $this->callback;
+        return Util::escapeForHtml($callback());
+    }
+
+    /**
+     * @param Exception|string $error
+     * @return string
+     */
+    protected function renderError($error)
+    {
+        return Util::renderError($error);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        try {
+            return $this->render();
+        } catch (Exception $e) {
+            return $this->renderError($e);
+        }
+    }
+}
---
+++ lib/ipl/Html/Renderable.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ipl\Html;
+
+interface Renderable
+{
+    public function render();
+}

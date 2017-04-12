4ece74b40afd02553e1ee12cb676002f82f3b6ad
Thomas Gelf <thomas@gelf.net>
2017-04-12 18:29:08 +0200

    ipl\Web\Component: some Icinga Web 2 components

---
+++ lib/ipl/Web/Component/Controls.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ipl\Web\Component;
+
+use ipl\Html\Container;
+
+class Controls extends Container
+{
+    protected $contentSeparator = "\n";
+
+    protected $defaultAttributes = array('class' => 'controls');
+}

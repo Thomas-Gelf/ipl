9bffebc18db4916d16aaaefc4268331da0f39224
Thomas Gelf <thomas@gelf.net>
2017-06-14 18:21:07 +0200

    CompatController: detach from Icinga Web 2...
    ...while trying to keep compatibility. Also implement new controls/content
    interfaces and inherit directly from ZF1

--- lib/ipl/Web/CompatController.php
+++ lib/ipl/Web/CompatController.php
@@ -3,48 +3,79 @@
 namespace ipl\Web;
 
 use Icinga\Application\Benchmark;
+use Icinga\Application\Config;
+use Icinga\Application\Icinga;
+use Icinga\Application\Modules\Manager;
+use Icinga\Application\Modules\Module;
 use Icinga\Authentication\Auth;
 use Icinga\Exception\IcingaException;
 use Icinga\Exception\ProgrammingError;
-use Icinga\Web\Controller as LegacyModuleController;
+use Icinga\Forms\AutoRefreshForm;
+use Icinga\Security\SecurityException;
+use Icinga\User;
 use Icinga\Web\Notification;
+use Icinga\Web\Session;
 use Icinga\Web\UrlParams;
 use Icinga\Web\Url as WebUrl;
-use ipl\Web\Component\ActionBar;
-use ipl\Web\Component\Content;
-use ipl\Web\Component\Controls;
-use ipl\Web\Component\Tabs;
+use Icinga\Web\Window;
+use ipl\Compat\Translator;
+use ipl\Translation\TranslationHelper;
+use ipl\Web\Component\ControlsAndContent;
+use ipl\Web\Controller\Extension\ControlsAndContentHelper;
 use ipl\Zf1\SimpleViewRenderer;
+use Zend_Controller_Action;
+use Zend_Controller_Action_HelperBroker as ZfHelperBroker;
 use Zend_Controller_Request_Abstract as ZfRequest;
 use Zend_Controller_Response_Abstract as ZfResponse;
-use Zend_Controller_Action_HelperBroker as ZfHelperBroker;
 
-class CompatController extends LegacyModuleController
+class CompatController extends Zend_Controller_Action implements ControlsAndContent
 {
-    /** @var Controls */
-    private $controls;
+    use TranslationHelper;
+    use ControlsAndContentHelper;
+
+    /** @var bool  Whether the controller requires the user to be authenticated */
+    protected $requiresAuthentication = true;
+
+    /** @var string The current module's name */
+    private $moduleName;
+
+    private $config;
+
+    private $configs = [];
+
+    private $module;
+
+    private $window;
 
     // https://github.com/joshbuchea/HEAD
 
-    /** @var Content */
-    private $content;
-
-    /** @var Url */
-    private $url;
-
-    /** @var Url */
-    private $originalUrl;
-
     /** @var SimpleViewRenderer */
-    private $viewRenderer;
+    protected $viewRenderer;
 
+    /** @var int|null */
     private $autorefreshInterval;
 
+    /** @var bool */
     private $reloadCss = false;
 
+    /** @var bool */
     private $rerenderLayout = false;
 
+    /** @var string */
     private $xhrLayout = 'inline';
+
+    /** @var \Zend_Layout */
+    private $layout;
+
+    /** @var string The inner layout (inside the body) to use */
+    private $innerLayout = 'body';
+
+    /**
+     * Authentication manager
+     *
+     * @var Auth|null
+     */
+    private $auth;
 
     /** @var UrlParams */
     protected $params;
@@ -75,9 +106,8 @@ class CompatController extends LegacyModuleController
 
         $this->handlerBrowserWindows();
         $moduleName = $this->getModuleName();
-        $this->view->translationDomain = $moduleName !== 'default' ? $moduleName : 'icinga';
-        // TODO: TranslationHelper::setTranslator(..);
-        $layout = $this->_helper->layout();
+        $this->initializeTranslator();
+        $layout = $this->layout = $this->_helper->layout();
         $layout->isIframe = $request->getUrl()->shift('isIframe');
         $layout->showFullscreen = $request->getUrl()->shift('showFullscreen');
         $layout->moduleName = $moduleName;
@@ -93,7 +123,7 @@ class CompatController extends LegacyModuleController
             $this->xhrLayout = $this->innerLayout;
         }
         if ($url->shift('_disableLayout')) {
-            $layout->disableLayout();
+            $this->layout->disableLayout();
         }
 
         // $auth->authenticate($request, $response, $this->requiresLogin());
@@ -103,11 +133,114 @@ class CompatController extends LegacyModuleController
             }
             $this->redirectToLogin(Url::fromRequest());
         }
+        if (($this->Auth()->isAuthenticated() || $this->requiresLogin())
+            && $this->getFrontController()->getDefaultModule() !== $this->getModuleName()) {
+            $this->assertPermission(Manager::MODULE_PERMISSION_NS . $this->getModuleName());
+        }
 
-        $this->view->tabs = new Tabs();
         Benchmark::measure('Ready to initialize the controller');
         $this->prepareInit();
         $this->init();
+    }
+
+    /**
+     * Prepare controller initialization
+     *
+     * As it should not be required for controllers to call the parent's init() method,
+     * base controllers should use prepareInit() in order to prepare the controller
+     * initialization.
+     *
+     * @see \Zend_Controller_Action::init() For the controller initialization method.
+     */
+    protected function prepareInit()
+    {
+    }
+
+    /**
+     * Return the current module's name
+     *
+     * @return  string
+     */
+    public function getModuleName()
+    {
+        if ($this->moduleName === null) {
+            $this->moduleName = $this->getRequest()->getModuleName();
+        }
+
+        return $this->moduleName;
+    }
+
+    public function Config($file = null)
+    {
+        if ($this->moduleName === null) {
+            if ($file === null) {
+                return Config::app();
+            } else {
+                return Config::app($file);
+            }
+        } else {
+            return $this->getModuleConfig($file);
+        }
+    }
+
+    public function getModuleConfig($file = null)
+    {
+        if ($file === null) {
+            if ($this->config === null) {
+                $this->config = Config::module($this->getModuleName());
+            }
+            return $this->config;
+        } else {
+            if (! array_key_exists($file, $this->configs)) {
+                $this->configs[$file] = Config::module($this->getModuleName(), $file);
+            }
+            return $this->configs[$file];
+        }
+    }
+
+    /**
+     * Return this controller's module
+     *
+     * @return  Module
+     */
+    public function Module()
+    {
+        if ($this->module === null) {
+            $this->module = Icinga::app()->getModuleManager()->getModule($this->getModuleName());
+        }
+
+        return $this->module;
+    }
+
+
+    public function Window()
+    {
+        if ($this->window === null) {
+            $this->window = new Window(
+                $this->_request->getHeader('X-Icinga-WindowId', Window::UNDEFINED)
+            );
+        }
+        return $this->window;
+    }
+
+    protected function handlerBrowserWindows()
+    {
+        if ($this->isXhr()) {
+            $id = $this->_request->getHeader('X-Icinga-WindowId', null);
+
+            if ($id === Window::UNDEFINED) {
+                $this->window = new Window($id);
+                $this->_response->setHeader('X-Icinga-WindowId', Window::generateId());
+            }
+        }
+    }
+
+    protected function initializeTranslator()
+    {
+        $moduleName = $this->getModuleName();
+        $domain = $moduleName !== 'default' ? $moduleName : 'icinga';
+        $this->view->translationDomain = $domain;
+        TranslationHelper::setTranslator(new Translator($domain));
     }
 
     public function init()
@@ -115,22 +248,70 @@ class CompatController extends LegacyModuleController
         // Hint: we intentionally do not call our parent's init() method
     }
 
-    public function getOriginalUrl()
+    /**
+     * Get the authentication manager
+     *
+     * @return Auth
+     */
+    public function Auth()
     {
-        if ($this->originalUrl === null) {
-            $this->originalUrl = clone($this->getUrlFromRequest());
+        if ($this->auth === null) {
+            $this->auth = Auth::getInstance();
         }
-
-        return clone($this->originalUrl);
+        return $this->auth;
     }
 
-    protected function getUrlFromRequest()
+    /**
+     * Whether the current user has the given permission
+     *
+     * @param   string  $permission Name of the permission
+     *
+     * @return  bool
+     */
+    public function hasPermission($permission)
     {
-        $webUrl = $this->getRequest()->getUrl();
+        return $this->Auth()->hasPermission($permission);
+    }
 
-        return Url::fromPath(
-            $webUrl->getPath()
-        )->setParams($webUrl->getParams());
+    /**
+     * Assert that the current user has the given permission
+     *
+     * @param   string  $permission     Name of the permission
+     *
+     * @throws  SecurityException       If the current user lacks the given permission
+     */
+    public function assertPermission($permission)
+    {
+        if (! $this->Auth()->hasPermission($permission)) {
+            throw new SecurityException('No permission for %s', $permission);
+        }
+    }
+
+    /**
+     * Return restriction information for an eventually authenticated user
+     *
+     * @param   string  $name   Restriction name
+     *
+     * @return  array
+     */
+    public function getRestrictions($name)
+    {
+        return $this->Auth()->getRestrictions($name);
+    }
+
+    /**
+     * Check whether the controller requires a login. That is when the controller requires authentication and the
+     * user is currently not authenticated
+     *
+     * @return  bool
+     */
+    protected function requiresLogin()
+    {
+        if (! $this->requiresAuthentication) {
+            return false;
+        }
+
+        return ! $this->Auth()->isAuthenticated();
     }
 
     public function prepareViewRenderer()
@@ -138,117 +319,6 @@ class CompatController extends LegacyModuleController
         $this->viewRenderer = new SimpleViewRenderer();
         $this->viewRenderer->replaceZendViewRenderer();
         $this->view = $this->viewRenderer->view;
-    }
-
-    /**
-     * @return Url
-     */
-    public function url()
-    {
-        if ($this->url === null) {
-            $this->url = $this->getOriginalUrl();
-        }
-
-        return $this->url;
-    }
-
-    /**
-     * @param $title
-     * @return $this
-     */
-    public function setTitle($title)
-    {
-        $title = $this->makeTitle(func_get_args());
-        $this->view->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * @param $title
-     * @return $this
-     */
-    public function addTitle($title)
-    {
-        $title = $this->makeTitle(func_get_args());
-        $this->view->title = $title;
-        $this->controls()->addTitle($title);
-
-        return $this;
-    }
-
-    private function makeTitle($args)
-    {
-        $title = array_shift($args);
-
-        if (empty($args)) {
-            return $title;
-        } else {
-            return vsprintf($title, $args);
-        }
-    }
-
-    /**
-     * @param $title
-     * @param null $url
-     * @param string $name
-     * @return $this
-     */
-    public function addSingleTab($title, $url = null, $name = 'main')
-    {
-        if ($url === null) {
-            $url = $this->url();
-        }
-
-        $this->tabs()->add($name, [
-            'label' => $title,
-            'url'   => $url,
-        ])->activate($name);
-
-        return $this;
-    }
-
-    /**
-     * TODO: Not sure whether we need dedicated Content/Controls classes,
-     *       a simple Container with a class name might suffice here
-     *
-     * @return Controls
-     */
-    public function controls()
-    {
-        if ($this->controls === null) {
-            $this->controls = $this->view->controls = Controls::create();
-        }
-
-        return $this->controls;
-    }
-
-    /**
-     * @return Tabs
-     */
-    public function tabs()
-    {
-        return $this->controls()->getTabs();
-    }
-
-    /**
-     * @return ActionBar
-     */
-    public function actions()
-    {
-        return $this->controls()->getActionBar();
-    }
-
-    /**
-     * @return Content
-     */
-    public function content()
-    {
-        if ($this->content === null) {
-            $this->content = $this->view->content = Content::create();
-        }
-
-        return $this->content;
     }
 
     /**
@@ -267,14 +337,14 @@ class CompatController extends LegacyModuleController
             );
         }
         $this->autorefreshInterval = $interval;
-        $this->_helper->layout()->autorefreshInterval = $interval;
+        $this->layout->autorefreshInterval = $interval;
         return $this;
     }
 
     public function disableAutoRefresh()
     {
         $this->autorefreshInterval = null;
-        $this->_helper->layout()->autorefreshInterval = null;
+        $this->layout->autorefreshInterval = null;
         return $this;
     }
 
@@ -300,10 +370,58 @@ class CompatController extends LegacyModuleController
         exit;
     }
 
+    /**
+     * @see Zend_Controller_Action::preDispatch()
+     */
+    public function preDispatch()
+    {
+        $form = new AutoRefreshForm();
+        $form->handleRequest();
+        $this->_helper->layout()->autoRefreshForm = $form;
+    }
+
+    /**
+     * Detect whether the current request requires changes in the layout and apply them before rendering
+     *
+     * @see Zend_Controller_Action::postDispatch()
+     */
+    public function postDispatch()
+    {
+        Benchmark::measure('Action::postDispatch()');
+
+        $layout = $this->layout;
+        $req = $this->getRequest();
+        $layout->innerLayout = $this->innerLayout;
+
+        /** @var User $user */
+        if ($user = $req->getUser()) {
+            if ((bool) $user->getPreferences()->getValue('icingaweb', 'show_benchmark', false)) {
+                if ($layout->isEnabled()) {
+                    $layout->benchmark = $this->renderBenchmark();
+                }
+            }
+
+            if (! (bool) $user->getPreferences()->getValue('icingaweb', 'auto_refresh', true)) {
+                $this->disableAutoRefresh();
+            }
+        }
+
+        if ($req->getParam('format') === 'pdf') {
+            $this->shutdownSession();
+            $this->sendAsPdf();
+            return;
+        }
+
+        if ($this->isXhr()) {
+            $this->postDispatchXhr();
+        }
+
+        $this->shutdownSession();
+    }
+
     public function postDispatchXhr()
     {
-        $layout = $this->_helper->layout();
-        $layout->setLayout($this->xhrLayout);
+        $this->layout->setLayout($this->xhrLayout);
         $resp = $this->getResponse();
 
         $notifications = Notification::getInstance();
@@ -319,14 +437,14 @@ class CompatController extends LegacyModuleController
             $resp->setHeader('X-Icinga-CssReload', 'now', true);
         }
 
-        if ($this->view->title) {
-            if (preg_match('~[\r\n]~', $this->view->title)) {
+        if ($this->title) {
+            if (preg_match('~[\r\n]~', $this->title)) {
                 // TODO: Innocent exception and error log for hack attempts
-                throw new IcingaException('No way, guy');
+                throw new IcingaException('No newlines allowed in page title');
             }
             $resp->setHeader(
                 'X-Icinga-Title',
-                rawurlencode($this->view->title . ' :: Icinga Web'),
+                rawurlencode($this->title . ' :: Icinga Web'),
                 true
             );
         } else {
@@ -343,6 +461,96 @@ class CompatController extends LegacyModuleController
 
         if ($name = $this->getModuleName()) {
             $this->getResponse()->setHeader('X-Icinga-Module', $name, true);
+        }
+    }
+
+    /**
+     * Redirect to login
+     *
+     * XHR will always redirect to __SELF__ if an URL to redirect to after successful login is set. __SELF__ instructs
+     * JavaScript to redirect to the current window's URL if it's an auto-refresh request or to redirect to the URL
+     * which required login if it's not an auto-refreshing one.
+     *
+     * XHR will respond with HTTP status code 403 Forbidden.
+     *
+     * @param   Url|string  $redirect   URL to redirect to after successful login
+     */
+    protected function redirectToLogin($redirect = null)
+    {
+        $login = Url::fromPath('authentication/login');
+        if ($this->isXhr()) {
+            if ($redirect !== null) {
+                $login->setParam('redirect', '__SELF__');
+            }
+
+            $this->_response->setHttpResponseCode(403);
+        } elseif ($redirect !== null) {
+            if (! $redirect instanceof Url) {
+                $redirect = Url::fromPath($redirect);
+            }
+
+            if (($relativeUrl = $redirect->getRelativeUrl())) {
+                $login->setParam('redirect', $relativeUrl);
+            }
+        }
+
+        $this->rerenderLayout()->redirectNow($login);
+    }
+
+    protected function sendAsPdf()
+    {
+        $pdf = new Pdf();
+        $pdf->renderControllerAction($this);
+    }
+
+    /**
+     * Render the benchmark
+     *
+     * @return string Benchmark HTML
+     */
+    protected function renderBenchmark()
+    {
+        $this->viewRenderer->postDispatch();
+        Benchmark::measure('Response ready');
+        return Benchmark::renderToHtml()/*
+            . '<pre style="height: 16em; vertical-overflow: scroll">'
+            . print_r(get_included_files(), 1)
+        . '</pre>'*/;
+    }
+
+    public function isXhr()
+    {
+        return $this->getRequest()->isXmlHttpRequest();
+    }
+
+    protected function redirectHttp($url)
+    {
+        if (! $url instanceof Url) {
+            $url = Url::fromPath($url);
+        }
+        $this->shutdownSession();
+        $this->_helper->Redirector->gotoUrlAndExit($url->getRelativeUrl());
+    }
+
+    /**
+     *  Redirect to a specific url, updating the browsers URL field
+     *
+     *  @param Url|string $url The target to redirect to
+     **/
+    public function redirectNow($url)
+    {
+        if ($this->isXhr()) {
+            $this->redirectXhr($url);
+        } else {
+            $this->redirectHttp($url);
+        }
+    }
+
+    protected function shutdownSession()
+    {
+        $session = Session::getSession();
+        if ($session->hasChanged()) {
+            $session->write();
         }
     }
 

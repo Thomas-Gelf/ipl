5af9e267af7212afa93c6c56c226c2957599f495
Thomas Gelf <thomas@gelf.net>
2017-06-14 17:02:10 +0200

    ValidHtml: add some documentation

--- lib/ipl/Html/ValidHtml.php
+++ lib/ipl/Html/ValidHtml.php
@@ -2,7 +2,20 @@
 
 namespace ipl\Html;
 
+/**
+ * Interface ValidHtml
+ *
+ * Implementations of this interface MUST guarantee, that the result of the
+ * render() method gives valid UTF-8 encoded HTML5.
+ */
 interface ValidHtml
 {
+    /**
+     * Renders to  HTML
+     *
+     * The result of this method is a valid UTF8-encoded HTML5 string.
+     *
+     * @return string
+     */
     public function render();
 }

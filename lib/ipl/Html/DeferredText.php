98aa2138a0640d3eb085ec4671b477c831ac217f
Thomas Gelf <thomas@gelf.net>
2017-06-14 17:02:46 +0200

    DeferredText: allow escaped content, add docs

--- lib/ipl/Html/DeferredText.php
+++ lib/ipl/Html/DeferredText.php
@@ -4,20 +4,43 @@ namespace ipl\Html;
 
 use Exception;
 
+/**
+ * This class allows to have plain text passed in as a callback. That way it
+ * would not be called and stringified unless it is going to be rendered and
+ * escaped to HTML
+ *
+ * Usage
+ * -----
+ * <code>
+ * $myVar = 'Some value';
+ * $text = new DeferredText(function () use ($myVar) {
+ *     return $myVar;
+ * });
+ * $myVar = 'Changed idea';
+ * echo $text;
+ * </code>
+ */
 class DeferredText implements ValidHtml
 {
-    /** @var callable */
+    /** @var callable will return the text that should be rendered */
     protected $callback;
 
+    /** @var bool */
     protected $escaped = false;
 
+    /**
+     * DeferredText constructor.
+     * @param callable $callback Must return the text that should be rendered
+     */
     public function __construct(callable $callback)
     {
         $this->callback = $callback;
     }
 
     /**
-     * @param callable $callback
+     * Static factory
+     *
+     * @param callable $callback Must return the text that should be rendered
      * @return static
      */
     public static function create(callable $callback)
@@ -25,25 +48,31 @@ class DeferredText implements ValidHtml
         return new static($callback);
     }
 
-    /**
-     * @return string
-     */
     public function render()
     {
         $callback = $this->callback;
-        return Util::escapeForHtml($callback());
+
+        if ($this->escaped) {
+            return $callback();
+        } else {
+            return Util::escapeForHtml($callback());
+        }
     }
 
     /**
-     * @param Exception|string $error
-     * @return string
+     * @param bool $escaped
+     * @return $this
      */
-    protected function renderError($error)
+    public function setEscaped($escaped = true)
     {
-        return Util::renderError($error);
+        $this->escaped = $escaped;
+        return $this;
     }
 
     /**
+     * Calls the render function, but is failsafe. In case an Exception occurs,
+     * an error is rendered instead of the expected HTML
+     *
      * @return string
      */
     public function __toString()
@@ -51,7 +80,7 @@ class DeferredText implements ValidHtml
         try {
             return $this->render();
         } catch (Exception $e) {
-            return $this->renderError($e);
+            return Util::renderError($e);
         }
     }
 }

6ad7fca43ccdc31c8a4c38b21cac3f389f173ed7
Thomas Gelf <thomas@gelf.net>
2017-06-20 21:38:02 +0200

    Link: allow access to constructor, add docs

--- lib/ipl/Html/Link.php
+++ lib/ipl/Html/Link.php
@@ -12,28 +12,39 @@ class Link extends BaseElement
     /** @var Url */
     protected $url;
 
-    protected function __construct()
+    /**
+     * Link constructor.
+     * @param $content
+     * @param $url
+     * @param null $urlParams
+     * @param array|null $attributes
+     */
+    public function __construct($content, $url, $urlParams = null, array $attributes = null)
     {
+        $this->setContent($content);
+        $this->setAttributes($attributes);
+        $this->attributes()->registerCallbackFor('href', array($this, 'getHrefAttribute'));
+        $this->setUrl($url, $urlParams);
     }
 
     /**
      * @param ValidHtml|array|string $content
      * @param Url|string $url
      * @param array $urlParams
-     * @param array $attributes
+     * @param mixed $attributes
      *
      * @return static
      */
     public static function create($content, $url, $urlParams = null, array $attributes = null)
     {
-        $link = new static();
-        $link->setContent($content);
-        $link->setAttributes($attributes);
-        $link->attributes()->registerCallbackFor('href', array($link, 'getHrefAttribute'));
-        $link->setUrl($url, $urlParams);
+        $link = new static($content, $url, $urlParams, $attributes);
         return $link;
     }
 
+    /**
+     * @param $url
+     * @param $urlParams
+     */
     public function setUrl($url, $urlParams)
     {
         if ($url instanceof WebUrl) { // Hint: Url is also a WebUrl
